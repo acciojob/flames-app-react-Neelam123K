f8c0acbc2d679342c104e9506429168ee557b1e9
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-08-04 15:13:36 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import FlamesGame from "./FlamesGame";
+import FlamesGame from "./FlamesApp";
 
 class App extends Component {
     render() {
